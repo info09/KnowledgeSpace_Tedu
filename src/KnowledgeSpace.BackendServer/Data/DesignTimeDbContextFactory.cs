d507902454edb4b13293e7272fb7b9129fd4a7f9
HuyTQ <huytq3103@gmail.com>
2022-03-03 15:07:38 +0700

    migration database

---
+++ src/KnowledgeSpace.BackendServer/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace KnowledgeSpace.BackendServer.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").AddJsonFile($"appsettings.{env}.json").Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectString);
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
